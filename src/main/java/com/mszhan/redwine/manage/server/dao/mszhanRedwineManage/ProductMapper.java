c19c03e6a091f7dcbb628d4ee3f6adfe99829147
wushutian <675557339@qq.com>
2018-04-07 11:32:06 +0800

    productMapper提交

--- src/main/java/com/mszhan/redwine/manage/server/dao/mszhanRedwineManage/ProductMapper.java
+++ src/main/java/com/mszhan/redwine/manage/server/dao/mszhanRedwineManage/ProductMapper.java
@@ -2,6 +2,18 @@ package com.mszhan.redwine.manage.server.dao.mszhanRedwineManage;
 
 import com.mszhan.redwine.manage.server.core.Mapper;
 import com.mszhan.redwine.manage.server.model.mszhanRedwineManage.Product;
+import com.mszhan.redwine.manage.server.model.mszhanRedwineManage.query.ProductQuery;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ProductMapper extends Mapper<Product> {
+
+    Integer queryCount(ProductQuery query);
+
+    List<Product> queryForPage(ProductQuery query);
+
+    void updatePic(@Param("id") Integer id, @Param("url") String url);
+
+    void removePicById(@Param("id") Integer id);
 }
